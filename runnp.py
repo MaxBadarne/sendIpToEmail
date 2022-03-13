af9118796b64060124cd227c7c5977299133965e
Max Badarne <45733957+CaptainSS3@users.noreply.github.com>
2022-03-13 18:21:22 +0100

    Add files via upload

---
+++ runnp.py
@@ -0,0 +1,24 @@
+import time
+from data import sendip
+from data import sendemail
+from data import private 
+import os
+import traceback
+import logging
+def run():
+	sendip.donp()
+	sendemail.sendip()
+	os.remove("temp.txt")                        
+	time.sleep(private.timetowait)
+	
+        
+while(True):
+	try:
+		time.sleep(1)
+		run()
+		
+
+	except Exception as e:
+    		logging.error(traceback.format_exc())
+    # Logs the error appropriately. 
+	
